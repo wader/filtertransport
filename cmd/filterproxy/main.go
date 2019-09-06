bd0d267b0121277ca1c92041671e7abd6f6b8cae
Mattias Wadman <mattias.wadman@gmail.com>
2019-09-06 11:36:51 +0200

    Move test programs to cmd

--- cmd/filterproxy/main.go
+++ cmd/filterproxy/main.go
@@ -4,12 +4,12 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/wader/filtertransport"
+	"github.com/wader/filtertransport/proxy"
 )
 
 func main() {
 	http.ListenAndServe("127.0.0.1:8080", http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
-		filtertransport.DefaultHandler.ServeHTTP(rw, r)
+		proxy.DefaultHandler.ServeHTTP(rw, r)
 	}))
 }
